7a134cc94e80c1b697b4b1d15f3a0ee384dabeae
Harshil2301 <harshilparmar871@gmail.com>
2024-09-26 11:57:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//popcorn size if-else
+
+let size = "S";
+
+if(size == "XL"){
+    console.log("Price is 250");
+}
+else if(size == "L"){
+    console.log("Price is 150");
+}
+else if(size == "M"){
+    console.log("Price is 100");
+}
+else{
+    console.log("Price is 50");
+}
